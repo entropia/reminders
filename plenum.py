34b16eac25810034f97e92975120a570c0ed23c5
Franziska Kunsmann <hi@kunsmann.eu>
2021-12-04 15:49:48 +0100

    plenum: support more date formats

--- plenum.py
+++ plenum.py
@@ -17,14 +17,13 @@ from email.mime.text import MIMEText
 from requests import get
 from smtplib import SMTP
 
-URL   = argv[1]
+URL = argv[1]
 
-DEBUG = environ.get('DEBUG', '0') == '1'
-DAYS  = int(environ.get('DELTA_DAYS', 1))
+DEBUG = environ.get("DEBUG", "0") == "1"
+DAYS = int(environ.get("DELTA_DAYS", 1))
 
 tomorrow = date.today() + timedelta(days=DAYS)
-start = '{} ==='.format(tomorrow.strftime('%Y-%m-%d'))
-end = '=== '
+
 
 def find_between(s, first, last):
     try:
@@ -34,32 +33,40 @@ def find_between(s, first, last):
     except ValueError:
         return
 
-setlocale(LC_ALL, 'de_DE.UTF-8')
 
-wiki = get(URL).content.decode('utf-8')
-plenum_tops = find_between(wiki, start, end)
+setlocale(LC_ALL, "de_DE.UTF-8")
+wiki = get(URL).content.decode("utf-8")
 
-if not plenum_tops:
+plenum_tops = None
+for date_format in ("%Y-%m-%d", "%d.%m.%Y"):
+    start = "{} ===".format(tomorrow.strftime(date_format))
+    plenum_tops = find_between(wiki, start, "=== ")
+
+    if plenum_tops:
+        break
+else:
     # Catch a corner case for the first plenum on a page
-    plenum_tops = find_between(wiki, start, '</textarea>')
+    plenum_tops = find_between(wiki, start, "</textarea>")
 
 if plenum_tops:
     template = """Hallo,
 morgen ist (laut Wiki) wieder mal Plenum. Nachfolgend die Tagesordnungs-
 punkte aus dem Wiki:
 
-{}""".format(plenum_tops.strip())
+{}""".format(
+        plenum_tops.strip()
+    )
 
     if DEBUG:
         print(template)
         exit(0)
 
     msg = MIMEText(template)
-    msg['Subject'] = 'Plenum am %s' % tomorrow.strftime('%A, %d.%m.%Y')
-    msg['From'] = argv[2]
-    msg['To'] = argv[3]
+    msg["Subject"] = "Plenum am %s" % tomorrow.strftime("%A, %d.%m.%Y")
+    msg["From"] = argv[2]
+    msg["To"] = argv[3]
 
-    smtpObj = SMTP('localhost')
+    smtpObj = SMTP("localhost")
     smtpObj.send_message(msg)
     smtpObj.quit()
 elif DEBUG:
